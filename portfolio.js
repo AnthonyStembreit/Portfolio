47512284245dd9ce375868c70a0788bc1eeeefee
Anthony Stematz-Breitling <stembreit@gmail.com>
2020-10-01 16:36:31 -0500

    transfered onclick function for project cards to contructor function

--- portfolio.js
+++ portfolio.js
@@ -1,26 +1,55 @@
 $(document).ready( function(){
-    console.log("hit start") 
- $("body").mouseenter(function(){
-    $("#cocktail-carousel").carousel('pause')
+     
+$("body").mouseenter(function(){
+    $(".carousel").carousel('pause')
 })
 
 let cardHeight = $(".d-block").css("height")
 $(".tech").css("height", cardHeight)
 
-$("#openCocktail").on("click", function(){
-    $("#cocktailDescription").attr("style", "height:100%")
-})
+function Card(showBtn, hideBtn, description, cardBody, carousel){
+this.show = showBtn
+this.hide = hideBtn
+this.description = description
+this.card = cardBody
+this.carousel = carousel
+}
+Card.prototype.openDescription = function (){
+    let descriptionBody = this.description
+    $(this.show).on("click", function(){
+        $(descriptionBody).attr("style", "height:100%")
+    })
+}
+Card.prototype.closeDescription = function (){
+    let descriptionBody = this.description
+    $(this.hide).on("click", function(){
+        $(descriptionBody).attr("style", "height:0")
+    })
+}
+Card.prototype.cycle = function (){
+    let carouselBody = this.carousel
+    $(this.card).mouseenter( function(){
+        $(carouselBody).carousel('next')
+        $(carouselBody).carousel('cycle')
+    })
+}
+Card.prototype.pause = function(){
+    let carouselBody = this.carousel
+    $(this.card).mouseleave(function(){
+        $(carouselBody).carousel('pause')
+    })
+}
+let cocktail = new Card("#openCocktail", "#closeCocktail", "#cocktailDescription", "#cocktail-body", "#cocktail-carousel")
+cocktail.openDescription()
+cocktail.closeDescription()
+cocktail.cycle()
+cocktail.pause()
 
-$("#closeCocktail").on("click", function(){
-    $("#cocktailDescription").attr("style", "height:0")
-})
-$("#cocktail-body").mouseenter( function(){
-    $("#cocktail-carousel").carousel('next')
-    $("#cocktail-carousel").carousel('cycle')
-})
-$("#cocktail-body").mouseleave(function(){
-    $("#cocktail-carousel").carousel('pause')
-})
+let exploration = new Card("#openExploration", "#closeExploration", "#explorationDescription", "#exploration-body", "#exploration-carousel")
+exploration.openDescription()
+exploration.closeDescription()
+exploration.cycle()
+exploration.pause()
 
 
 })
