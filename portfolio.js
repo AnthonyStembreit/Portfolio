42786096925c8f0d4d23d6b09240a670becb8fa5
Anthony Stematz-Breitling <stembreit@gmail.com>
2020-09-30 19:44:04 -0500

    was able to find out how to pause and start cycle for coding quiz carousel however now having issues with it starting on load rather than static

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,8 @@
+$( document ).ready( function(){
+    console.log("hit start")
+    $("#quiz-carousel").carousel('pause')
+
+
 $("#openQuiz").on("click", function(){
     $("#quizDescription").attr("style", "height:100%")
 })
@@ -6,17 +11,17 @@ $("#closeQuiz").on("click", function(){
     console.log("click")
     $("#quizDescription").attr("style", "height:0")
 })
-
-
-$("#quiz-body").mouseenter( async function(){
+$("#quiz-body").mouseenter( function(){
     console.log("hit in")
-   await $("#quiz-carousel").attr("data-interval", "5000")
+    
+    $("#quiz-carousel").carousel('next')
     $("#quiz-carousel").carousel('cycle')
-    console.log($("#quiz-carousel").attr("data-interval"))
+
 })
 $("#quiz-body").mouseleave(function(){
     console.log("hit out")
-    $("#quiz-carousel").attr("data-interval", "false")
+
     $("#quiz-carousel").carousel('pause')
-    console.log($("#quiz-carousel").attr("data-interval"))
+  
+})
 })
