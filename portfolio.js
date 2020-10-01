18dcbc747a5610445cc75c01831885592ce0bdbd
Anthony Stematz-Breitling <stembreit@gmail.com>
2020-09-30 20:58:04 -0500

    found a creative solution to pause the carousel cycle by listening to the body and and activating carousel pause on mouseenter

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,8 @@
-$( document ).ready( function(){
-    console.log("hit start")
-    $("#quiz-carousel").carousel('pause')
+$(document).ready( function(){
+    console.log("hit start") 
+    $("body").mouseenter(function(){
+        $("#quiz-carousel").carousel('pause')
+    })
 
 
 $("#openQuiz").on("click", function(){
@@ -13,7 +15,7 @@ $("#closeQuiz").on("click", function(){
 })
 $("#quiz-body").mouseenter( function(){
     console.log("hit in")
-    
+
     $("#quiz-carousel").carousel('next')
     $("#quiz-carousel").carousel('cycle')
 
