339cfc44cb93df1d6465a3ba082ac0803f6dc3e1
Anthony Stematz-Breitling <stembreit@gmail.com>
2020-10-01 15:23:58 -0500

    made a folder for images, added updated resume, changed card names to match cocktail project

--- portfolio.js
+++ portfolio.js
@@ -1,32 +1,25 @@
 $(document).ready( function(){
     console.log("hit start") 
  $("body").mouseenter(function(){
-    $("#quiz-carousel").carousel('pause')
+    $("#cocktail-carousel").carousel('pause')
 })
 
 let cardHeight = $(".d-block").css("height")
 $(".tech").css("height", cardHeight)
 
-$("#openQuiz").on("click", function(){
-    $("#quizDescription").attr("style", "height:100%")
+$("#openCocktail").on("click", function(){
+    $("#cocktailDescription").attr("style", "height:100%")
 })
 
-$("#closeQuiz").on("click", function(){
-    console.log("click")
-    $("#quizDescription").attr("style", "height:0")
+$("#closeCocktail").on("click", function(){
+    $("#cocktailDescription").attr("style", "height:0")
 })
-$("#quiz-body").mouseenter( function(){
-    console.log("hit in")
-
-    $("#quiz-carousel").carousel('next')
-    $("#quiz-carousel").carousel('cycle')
-
+$("#cocktail-body").mouseenter( function(){
+    $("#cocktail-carousel").carousel('next')
+    $("#cocktail-carousel").carousel('cycle')
 })
-$("#quiz-body").mouseleave(function(){
-    console.log("hit out")
-
-    $("#quiz-carousel").carousel('pause')
-  
+$("#cocktail-body").mouseleave(function(){
+    $("#cocktail-carousel").carousel('pause')
 })
 
 
