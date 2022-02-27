1c2ba515b4df30b05ce3d1e5205f95f017be0af6
Anthony Stematz-Breitling <stembreit@gmail.com>
2022-02-26 23:03:47 -0600

    created animated project cards dynamically rendering them to page from projectArray of project Objects, updated nav and footer styles for color scheme

--- index.js
+++ index.js
@@ -1,36 +1,133 @@
-//when my name is clicked
-//remove the class hide from all sections
-//when about is clicked
-//remove class hide from about section and add class hide to contact and project sections
-//when about is clicked
-//remove class hide from about section and add class hide to contact and project sections
 const aboutSection = $("#bio")
-const contactSection =  $("#contact")
+const contactSection = $("#contact")
 const projectSection = $("#projects")
-
-$("#about-nav").click(e => {
+const aboutNavLink = $("#about-nav")
+const contactNavLink = $("#contact-nav")
+const projectNavLink = $("#project-nav")
+//Navigation
+function changePage(show, hide1, hide2){
+    show.removeClass("hide");
+    hide1.addClass("hide");
+    hide2.addClass("hide");
+}
+function changeActive(active, deactivate1, deactivate2){
+    active.addClass("active-nav");
+    deactivate1.removeClass("active-nav");
+    deactivate2.removeClass("active-nav");
+}
+aboutNavLink.click(e => {
     e.preventDefault();
-    aboutSection.removeClass("hide");
-    contactSection.addClass("hide");
-    projectSection.addClass("hide")
+   changePage(aboutSection, contactSection, projectSection)
+   changeActive(aboutNavLink, contactNavLink, projectNavLink)
 })
 
-$("#contact-nav").click(e => {
+contactNavLink.click(e => {
     e.preventDefault();
-    contactSection.removeClass("hide");
-    aboutSection.addClass("hide");
-    projectSection.addClass("hide")
+    changePage(contactSection, aboutSection, projectSection)
+    changeActive(contactNavLink, projectNavLink, aboutNavLink)
 })
 
-$("#project-nav").click(e => {
+projectNavLink.click(e => {
     e.preventDefault();
-    projectSection.removeClass("hide");
-    aboutSection.addClass("hide");
-    contactSection.addClass("hide")
+    changePage(projectSection, aboutSection, contactSection)
+    changeActive(projectNavLink, aboutNavLink, contactNavLink)
 })
 
 $("#name").click(e => {
     projectSection.removeClass("hide");
     contactSection.removeClass("hide");
     aboutSection.removeClass("hide");
+
+    aboutNavLink.removeClass("active-nav");
+    contactNavLink.removeClass("active-nav");
+    projectNavLink.removeClass("active-nav");
 })
+
+//Display Project Cards
+for (i = 0; i < projectArr.length; i++) {
+    //object deconstruction
+    let { title, github, deployed, img1, alt1, img2, alt2, img3, alt3, img4, alt4, img5, alt5 } = projectArr[i]
+    //template literal of project card
+    let projectCard = `<div id="${i}-card" class="projectCard">
+         <p class="card-title">${title}</p>
+      
+         <div id="${i}-container" class="img-container">
+
+             <!--There needs to be 4 bc of the transistion effects-->
+             <img id="${i}-img1" class="reset overlay img1" src=${img1}  alt=${alt1} />
+             <img id="${i}-img2" class="reset img2" src=${img2}  alt=${alt2} />
+             
+             <div id="${i}-img3" class="reset img3">
+                 <img class="flip-box-front" src=${img3}  alt=${alt3} />
+                 <img id="${i}-img4" class="flip-box-back img4" src=${img4} alt=${alt4} />
+             </div>
+
+             <img id="${i}-img5" class="reset img5" src=${img5} alt=${alt5} />
+             
+             <div id="${i}-final" class="reset final-pg">
+                 <a href=${deployed}>Deplyed</a>
+                 <a href=${github}>Github</a>
+                 <!--but not on these buttons here-->
+                 <button id="${i}-overview">Overview</button>
+                 <button id="${i}-replay">Replay</button>
+             </div> 
+
+         </div>
+     </div>`
+    //appending project card to page
+    projectSection.append(projectCard)
+    let imgArr = [$(`#${i}-img1`), $(`#${i}-img2`), $(`#${i}-img3`), $(`#${i}-img4`), $(`#${i}-img5`), $(`#${i}-final`)]
+    revealEL(imgArr[0])
+    //event listeners for project card
+
+    //img container hover
+    $(`#${i}-container`).mouseover(async e => {
+        e.preventDefault();
+        console.log("hover " + e.target.id)
+        if (imgArr[4].attr("class").indexOf("overlay") === -1) {
+            animateImgs(imgArr)
+        }
+    })
+    //replay animation on click
+    $(`#${i}-replay`).click(e => {
+        e.preventDefault();
+        imgArr[5].attr("style", "opacity: 0")
+        imgArr[2].removeClass("flip-box-inner flip-now")
+        for (i = 1; i < imgArr.length; i++) {
+            resetEl(imgArr[i])
+        }
+        revealEL(imgArr[0])
+        animateImgs(imgArr)
+    })
+    //overview on click
+    $(`#${i}-overview`).click(e => {
+        e.preventDefault();
+        console.log(e.target.id)
+    })
+}
+function animateImgs(imgArr) {
+    let currentImg = 0;
+    let imgInterval = setInterval(function () {
+        currentImg++;
+        if(currentImg >= 5){
+            console.log(currentImg)
+            clearInterval(imgInterval);
+            resetEl(imgArr[0])
+            imgArr[5].attr("style", "opacity: 1")
+            return;
+        }
+        if (imgArr[currentImg] === imgArr[3]) {
+            imgArr[2].addClass("flip-box-inner flip-now")
+        }
+        revealEL(imgArr[currentImg])
+    }, 1000);
+}
+function resetEl(element) {
+    element.attr("style", "transition: unset;")
+    element.removeClass("overlay")
+}
+function revealEL(element) {
+    element.attr("style", "transition: .8s ease;")
+    element.addClass("overlay")
+}
+
---
+++ projects.js
@@ -0,0 +1,56 @@
+const projectArr = [
+    {
+        title: "Cocktail_Coterie",
+        github: "https://github.com/AuroraBrune/cocktail_coterie",
+        deployed: "https://cocktail-coterie.herokuapp.com/",
+        img1: "./images/ProjectOne/search.png",
+        alt1: "search by drink name or ingrediants", 
+        img2: "./images/ProjectOne/searchResults.png",
+        alt2: "search results via cocktail cards with drink info",
+        img3: "./images/ProjectOne/newParty.png",
+        alt3: "form with multiple inputs for your party info",
+        img4: "./images/ProjectOne/Invitation.png",
+        alt4: "invitation with unique url that you can send to friends",
+        img5: "./images/ProjectOne/home.png",
+        alt5: "homepage with desription of application"
+    },
+    {
+        title: "p2",
+        github: "gh2",
+        deployed: "deployed2",
+        img1: "img-2.1",
+        alt1: "alt-2.1", 
+        img2: "img-2.2",
+        alt2: "alt-2.2",
+        img3: "img-2.3",
+        alt3: "alt-2.3",
+        img4: "img-2.4",
+        alt4: "alt-2.4"
+    },
+    {
+        title: "p3",
+        github: "gh3",
+        deployed: "deployed3",
+        img1: "img-3.1",
+        alt1: "alt-3.1", 
+        img2: "img-3.2",
+        alt2: "alt-3.2",
+        img3: "img-3.3",
+        alt3: "alt-3.3",
+        img4: "img-3.4",
+        alt4: "alt-3.4"
+    },
+    {
+        title: "p3",
+        github: "gh3",
+        deployed: "deployed3",
+        img1: "img-3.1",
+        alt1: "alt-3.1", 
+        img2: "img-3.2",
+        alt2: "alt-3.2",
+        img3: "img-3.3",
+        alt3: "alt-3.3",
+        img4: "img-3.4",
+        alt4: "alt-3.4"
+    }
+]
